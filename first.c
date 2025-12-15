44ad0593263aaafe269f2538f3e6b024ee876c00
Paresh <garvitinfo.paresh@gmail.com>
2025-12-15 09:48:09 +0530

    15-12-2025

--- first.c
+++ first.c
@@ -29,21 +29,22 @@ int main()
 
 
     int a=97;
-    printf("\n1. : %d",--a);        //    
-    printf("\n2. : %d",--a);        //
-    printf("\n3. : %d",a++);        //    
-    printf("\n4. : %d",--a);        //
-    printf("\n5. : %d",a++);        //
-    printf("\n6. : %d",a--);        //
-    printf("\n7. : %d",--a);        //
-    printf("\n8. : %d",a++);        //
-    printf("\n9. : %d",a--);        //
-    printf("\nX. : %d",a);          //
+    // printf("\n1. : %d",--a);        //    
+    // printf("\n2. : %d",--a);        //
+    // printf("\n3. : %d",a++);        //    
+    // printf("\n4. : %d",--a);        //
+    // printf("\n5. : %d",a++);        //
+    // printf("\n6. : %d",a--);        //
+    // printf("\n7. : %d",--a);        //
+    // printf("\n8. : %d",a++);        //
+    // printf("\n9. : %d",a--);        //
+    // printf("\nX. : %d",a);          //
 
 
 
 
-
+    printf("\n %d %d %d ", a++,++a,a);  
+    // 97   99     99
 
 
 
