88fb8ed3344be675302fc9028db2c8f72fef828e
Paresh <garvitinfo.paresh@gmail.com>
2025-12-15 08:56:21 +0530

    15-12-2025

---
+++ first.c
@@ -0,0 +1,62 @@
+// 
+// 
+
+#include<stdio.h>
+int main()
+{
+    // int a=10,b=100,c=1000;
+    // if(a>b && a>c)
+    // {
+    //     printf("a is greater.");
+    // }
+    // else if(b>c)
+    // {
+    //     printf("b is greater.");
+    // }
+    // else
+    // {
+    //     printf("c is greater.");
+    // }
+
+    // unary operator
+    //                  ++                              --
+    //              inc by  1                        dec by 1
+    // a=10         a=a+1                            a=a-1
+    //              a=11                             a=9  
+    //  pre inc                post inc |   pre dec             post dec
+    //  ++a                         a++ |       --a             a--
+    // 
+
+
+    int a=97;
+    printf("\n1. : %d",--a);        //    
+    printf("\n2. : %d",--a);        //
+    printf("\n3. : %d",a++);        //    
+    printf("\n4. : %d",--a);        //
+    printf("\n5. : %d",a++);        //
+    printf("\n6. : %d",a--);        //
+    printf("\n7. : %d",--a);        //
+    printf("\n8. : %d",a++);        //
+    printf("\n9. : %d",a--);        //
+    printf("\nX. : %d",a);          //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
